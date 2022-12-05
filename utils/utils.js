0bab8149f158ba172d761ec984142a73625c6dd3
Eugene Zorin <ezorin5@yandex.ru>
2022-12-05 23:54:09 +0400

    refactor

--- utils/utils.js
+++ utils/utils.js
@@ -7,8 +7,16 @@ function handleLog(err) {
 }
 
 const handleError = (err, next) => {
-  if (err instanceof Error || err.code === 11000) {
-    const currentError = err instanceof Error ? new BadRequestError() : new ConflictError();
+  if (err instanceof Error || err.name === 'MongoServerError') {
+    let currentError;
+
+    if (err instanceof Error) {
+      currentError = new BadRequestError();
+    } else if (err.name === 'MongoServerError' && err.code === 11000) {
+      console.log(err.code)
+      currentError = new ConflictError();
+    }
+
     next(currentError);
   } else {
     next(err);
